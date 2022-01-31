4f5caf99f02bf5c241f64c66a6cbd59242b0df0d
RaymondWatkin <93054317+RaymondWatkin@users.noreply.github.com>
2022-01-31 16:43:10 -0500

    Update cachematrix.R
    Copied code I wrote for cacheMatrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#creates augmented "matrix" structure which includes place to cache its inverse
+makeCacheMatrix <- function(M = matrix()){
+  invM <- NULL
+  set <- function(N){
+    M <<- N
+    invM <<- NULL
+  }
+  get <- function() M
+  setInv <- function(solve) invM <<- solve
+  getInv <- function() invM
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#checks if inverse of matrix has been calculated and, if not, calculates inverse
+#and caches it in augmented matrix structure
+cacheSolve <- function(M,...){
+  invM <- M$getInv()
+  
+  if(!is.null(invM)){
+    message("getting cached data")
+    return(invM)
+  }
+  data <- M$get()
+  invM <- solve(data,...)
+  M$setInv(invM)
+  invM
 }
